f68fdc695716956d94ad8d4ffc5d8958c548e8f1
Chun Shen <chunshen1987@gmail.com>
2018-03-23 16:57:39 -0400

    some clean up in the parameter class

--- src/Parameters.cpp
+++ src/Parameters.cpp
@@ -6,18 +6,10 @@
 
 namespace MCGlb {
 
-void Parameters::set_parameters() {
-    set_b(2.0);
-}
-
 void Parameters::set_b(real b_in) {
     std::ostringstream param_val;
     param_val << b_in;
     set_parameter("b", param_val.str());
-}
-
-real Parameters::get_b() {
-    return(static_cast<real>(std::stod(get_param_val("b"))));
 }
 
 }
--- src/Parameters.h
+++ src/Parameters.h
@@ -7,39 +7,20 @@
 #include "ParametersMap.h"
 #include "data_structs.h"
 
+using std::string;
+
 namespace MCGlb {
 
 class Parameters : public ParametersMap {
- private:
-    std::string parameter_filename;
-    real b;
-
-    std::string projectile_nucleus_name;
-    std::string target_nucleus_name;
-
  public:
     Parameters() = default;
     ~Parameters() {};
 
-    void set_parameters();
-
-    void set_parameter_filename(std::string input_filename) {
-        parameter_filename = input_filename;
-    }
-
     void set_b(real b_in);
-    real get_b();
+    real get_b() {return(static_cast<real>(std::stod(get_param_val("b"))));}
 
-    void set_projectile_nucleus(std::string nucleus_name) {
-        projectile_nucleus_name = nucleus_name;
-    }
-    std::string get_projectle_nucleus_name() {return(projectile_nucleus_name);}
-    
-    void set_target_nucleus(std::string nucleus_name) {
-        target_nucleus_name = nucleus_name;
-    }
-    std::string get_target_nucleus_name() {return(target_nucleus_name);}
-
+    string get_projectle_nucleus_name() {return(get_param_val("Projectile"));}
+    string get_target_nucleus_name() {return(get_param_val("Target"));}
 };
 
 }
