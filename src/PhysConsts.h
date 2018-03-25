fa1e3343dea032c5de5f7223493522e5c368260b
Chun Shen <chunshen1987@gmail.com>
2018-03-25 15:17:02 -0400

    add PhysConsts namespace to the physics constants

---
+++ src/PhysConsts.h
@@ -0,0 +1,14 @@
+// Copyright @ Chun Shen 2018
+
+#ifndef SRC_PHYSCONSTS_H_
+#define SRC_PHYSCONSTS_H_
+
+namespace PhysConsts {
+
+const float HBARC         = 0.19733;  // GeV*fm
+const float MProton       = 0.938;    // GeV
+const float MQuarkValence = 0.340;    // GeV
+
+}
+
+#endif   // SRC_PHYS_CONST_H_
