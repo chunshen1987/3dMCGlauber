c5e98219861b97c23b49c850c69c5cea1b414968
Chun Shen <chunshen1987@gmail.com>
2018-05-24 17:57:52 -0400

    some small improvement

--- test/run_all_UnitTests.sh
+++ test/run_all_UnitTests.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+cp -r ../eps09 ./
+
 for iexe in `ls | grep "unittest"`
 do
     echo "$(tput bold)$(tput setaf 5)Running Unit Tests for $iexe ... $(tput setaf 0)"
     ./$iexe
 done
+
+rm -fr eps09
