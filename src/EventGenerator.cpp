8a9448ac3bc9c51384f99952b1133ce032c9f44c
Chun Shen <chunshen1987@gmail.com>
2018-05-31 15:42:35 -0400

    some improvement

--- src/EventGenerator.cpp
+++ src/EventGenerator.cpp
@@ -28,13 +28,21 @@ void EventGenerator::generate_events() {
     record_file << "# event_id  Npart  Ncoll  Nstrings  b(fm)" << std::endl;
 
     int iev = 0;
+    int nev_progress = nev/10;
+    int mean_Npart = 0;
     while (iev < nev) {
         mc_glauber_ptr->make_nuclei();
         auto Ncoll = mc_glauber_ptr->make_collision_schedule();
         auto Npart = mc_glauber_ptr->get_Npart();
         auto Nstrings = mc_glauber_ptr->decide_QCD_strings_production();
         if (event_of_interest_trigger(Npart, Ncoll, Nstrings)) {
+            mean_Npart += Npart;
+            if (iev%nev_progress == 0) {
+                std::cout << "Progress: " << iev << " out of " << nev
+                          << " is done." << std::endl;
+            }
             iev++;
+
             Ncoll = mc_glauber_ptr->perform_string_production();
 
             if (!statistics_only) {
@@ -50,6 +58,8 @@ void EventGenerator::generate_events() {
         }
     }
     record_file.close();
+    mean_Npart = static_cast<real>(mean_Npart)/static_cast<real>(nev);
+    std::cout << "Completed. <Npart> = " << mean_Npart << std::endl;
 }
 
 
