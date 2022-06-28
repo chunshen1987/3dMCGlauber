2dd286950c3c8a8afabe02339a99391d9bea13b2
Chun Shen <chunshen1987@gmail.com>
2022-06-28 16:53:40 -0400

    adding new unit test for the parameter class

--- src/ParametersMap_UnitTest.cpp
+++ src/ParametersMap_UnitTest.cpp
@@ -3,11 +3,13 @@
 #include "ParametersMap.h"
 #include <iostream>
 
+
 TEST_CASE("Test set and get parameters in ParametersMap") {
     ParametersMap parameter_list;
     parameter_list.set_parameter("test", "1.0");
     CHECK(parameter_list.get_param_val("test") == "1.0");
 }
+
 
 TEST_CASE("Test read in parameters from a file") {
     ParametersMap parameter_list;
@@ -19,4 +21,14 @@ TEST_CASE("Test read in parameters from a file") {
     CHECK(parameter_list.get_param_val("roots") == "200");
     CHECK(parameter_list.get_param_val("useQuarks") == "2");
     CHECK(parameter_list.get_param_val("QCD_string_production_mode") == "1");
+}
+
+
+TEST_CASE("Test default parameter value") {
+    ParametersMap parameter_list;
+    CHECK(parameter_list.get_param_double("test", 2.0) == 2.0);
+    CHECK(parameter_list.get_param_int("test2", 11) == 11);
+    parameter_list.set_parameter("test", "34.0");
+    CHECK(parameter_list.get_param_int("test", 3) == 34);
+    CHECK(parameter_list.get_param_double("test", 5) == 34);
 }
