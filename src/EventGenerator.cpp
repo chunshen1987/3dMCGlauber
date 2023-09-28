83a72ec7fe06594b9a28a46cb5bd658873b10a14
Gregoire Pihan <g.pihan@gmail.com>
2023-09-28 12:11:54 -0400

    Avoid having both binary and txt into the same file.

--- src/EventGenerator.cpp
+++ src/EventGenerator.cpp
@@ -159,7 +159,7 @@ void EventGenerator::generate_events(int nev, int event_id_offset) {
                 density_maker_ptr_->output_energyDensity_eta_distribution(
                         "ed_etas_distribution", event_id, 1);
             }
-            if (batchDensityOutput_) {
+            else if (batchDensityOutput_) {
                 density_maker_ptr_->output_netBaryon_eta_distribution(
                         "nB_etas_distribution", event_id);
                 density_maker_ptr_->output_netElectricCharges_eta_distribution(
