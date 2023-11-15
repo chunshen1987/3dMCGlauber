1116e40d8ada16bd95d2f77a85061781e229dee6
Chun Shen <chunshen1987@gmail.com>
2023-11-15 12:32:36 -0500

    formatting

--- src/EventGenerator.cpp
+++ src/EventGenerator.cpp
@@ -153,11 +153,10 @@ void EventGenerator::generate_events(int nev, int event_id_offset) {
                 density_maker_ptr_->output_netBaryon_eta_distribution(
                         "nB_etas_distribution", event_id, 1);
                 density_maker_ptr_->output_netElectricCharges_eta_distribution(
-                        "nQ_etas_distribution", event_id, 1); /// Changer fonction pour accepter boolean integer/frac
+                        "nQ_etas_distribution", event_id, 1);
                 density_maker_ptr_->output_energyDensity_eta_distribution(
                         "ed_etas_distribution", event_id, 1);
-            }
-            else if (batchDensityOutput_) {
+            } else if (batchDensityOutput_) {
                 density_maker_ptr_->output_netBaryon_eta_distribution(
                         "nB_etas_distribution", event_id);
                 density_maker_ptr_->output_netElectricCharges_eta_distribution(
