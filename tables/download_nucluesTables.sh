89efd9d917162e9095fab7c7d446befc3c0bf494
Chun Shen <chunshen@wayne.edu>
2024-06-03 09:59:37 -0400

    remove the nucleus configuration tables out of the repo
    use a bash script to download the nucleus configuration from an exteral
    repo.

---
+++ tables/download_nucluesTables.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+USERNAME="wayne_state_nuclear_theory"
+REPO_SLUG="GlauberNucleusConfigs"
+BRANCH="main"
+
+# Bitbucket API base url
+API_BASE_URL="https://api.bitbucket.org/2.0"
+
+# Define file name
+FILE_PATH="nucleusConfigurations.tar.gz"
+# Set the name of URL
+API_FILE_URL="${API_BASE_URL}/repositories/${USERNAME}/${REPO_SLUG}/src/${BRANCH}/${FILE_PATH}"
+
+curl -L -o "${FILE_PATH}" "${API_FILE_URL}"
+
+tar -xf ${FILE_PATH}
+rm -fr ${FILE_PATH}
+mv nucleusConfigurations/*.bin.in .
+rm -fr nucleusConfigurations
