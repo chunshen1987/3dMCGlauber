866acf0a48951f92fb7b58a2ab312a9e568361b6
Gregoire Pihan <g.pihan@gmail.com>
2024-07-22 11:23:41 -0400

    Finalized ML driver script.

--- utilities/MLSimsDriver.py
+++ utilities/MLSimsDriver.py
@@ -9,8 +9,44 @@ import shutil
 import subprocess, sys
 import numpy as np
 import pickle
-import sys
-import tensorflow as tf
+import importlib
+import string
+import os
+import random
+
+def printHelp() -> None:
+    print("Usage: {} nev path_to_parameters path_to_emulator_folder [output_file_name] [output_folder_name]".format(sys.argv[0]))
+    print("nev: the number of initial stage events from 3DMCGlauber")
+    print("path_to_parameters: The parameter dictionnary for 3DMCGlauber")
+    print("path_to_emulator_folder: the path to the emulator folder containing the emulator object and the object definition.")
+    print("output_file_name: [optional] output name for the output pickle dictionnary containing the predictions.")
+    print("Default output_file_name: OUTPUT_{Proj}{Targ}_{roots}_{Nev}.dat")
+    print("output_folder: [optional] Folder in which the initial stage data and predictions are sent to.")
+    print("Default output_folder: {Proj}{Targ}_{roots}_{Nev}")
+
+def dynamic_import(module_name, file_path):
+    try:
+        # Load the module from the given file path
+        spec = importlib.util.spec_from_file_location(module_name, file_path)
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[module_name] = module
+        spec.loader.exec_module(module)
+        return module
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+def create_folder(folder_path):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        return folder_path
+    else:
+        random_char = random.choice(string.ascii_letters)
+        folder_path_ = folder_path+"_"+str(random_char)
+        return create_folder(folder_path_)
 
 def save_data(X, Arr, fname="out") -> None:
     """ Data is saved as a 2D numpy array.
@@ -22,15 +58,17 @@ def save_data(X, Arr, fname="out") -> None:
     pickle.dump(OUT, fi)
     fi.close()
 
-def Predict(ARR, path_to_emulator_object, out_fname="Predictions", Nmax=-1) -> None:
+def Predict(ARR, path_to_emulator_object, out_fname="Predictions", out_Fname="PRED", Nmax=-1) -> None:
     """ 1) Load emulator pickle object. 
         The emulator object needs:
         - a predict method taking data as input and output predictions 
         2) Perform the prediction of the desired 2D data ARR
     """
     # Load emulator
+    h = os.getcwd()
+    os.chdir(path_to_emulator_object)
     try:
-        with open(path, "rb") as pf:
+        with open("EM.dat", "rb") as pf:
             EM = pickle.load(pf)
     except FileNotFoundError:
         print("Can't find emulator object at path: "+path)
@@ -38,16 +76,19 @@ def Predict(ARR, path_to_emulator_object, out_fname="Predictions", Nmax=-1) -> N
 
     N = len(ARR) - 1 if Nmax == -1 else Nmax
     X, INPUT_DATA = ARR[0], ARR[1:N]
-    save_data(X, EM.predict(INPUT_DATA), fname=out_fname)
+    pred = EM.predict(INPUT_DATA)
+    save_data(np.linspace(np.min(X), np.max(X), pred.shape[1]), pred, fname=out_Fname+"/"+out_fname)
+    os.chdir(h)
 
-def generate_3DMCGlauber_input(nev, paraDict):
+def Generate_3DMCGlauber_input(nev, paraDict, Folder_path):
     """ This function generates nev initial stage 
         events using 3DMCGlauber set on paraDict parameters
     """
-    command = "./3dMCGlb.e {} input -1 batch_density_output=1".format(nev)
+    h = os.getcwd()
+    os.chdir(os.path.abspath(Folder_path))
+    command = "./3DMCG {} input -1 batch_density_output=1".format(nev)
     for ikey in paraDict.keys():
         command += " {}={}".format(ikey, paraDict[ikey])
-
     subprocess.run(command, shell = True, executable="/bin/bash")
 
     edArr = np.fromfile("ed_etas_distribution_N_72.dat", dtype="float32")
@@ -57,65 +98,66 @@ def generate_3DMCGlauber_input(nev, paraDict):
     edArr = edArr.reshape(-1, 72)
     nBArr = nBArr.reshape(-1, 72)
     nQArr = nQArr.reshape(-1, 72)
+    os.chdir(h)
     return edArr, nBArr, nQArr
 
+def PrepareFolder(path_to_emulator, Fname_out):
+    Folder_path = create_folder(path_to_emulator+"/"+Fname_out) 
+    Fname_out = Folder_path.split("/")[-1]
+    h = os.getcwd()
+    shutil.copyfile(path_to_parameters, Folder_path+"/parameters.py")
+    shutil.copyfile(h+"/input", Folder_path+"/input")
+    os.system("ln -s "+h+"/3dMCGlb.e " +os.path.abspath(Folder_path)+"/3DMCG")
+    os.system("ln -s "+h+"/Metropolis.e " +os.path.abspath(Folder_path)+"/Metropolis.e")
+    os.system("ln -s "+h+"/tables " +os.path.abspath(Folder_path)+"/tables")
+    return Folder_path, Fname_out
 
-def printHelp() -> None:
-    print("Usage: {} nev path_to_emulator_object output_file_name".format(sys.argv[0]))
-    print("nev: the number of initial stage events from 3DMCGlauber")
-    print("path to emulator: the path to the emulator object")
-    print("output_file_name: output name for the output pickle dictionnary containing the predictions.")
-
-
+def CleanTemporary(Folder_path) -> None:
+    os.system("rm -rf __pycache__")
+    os.system("rm -rf "+os.path.abspath(Folder_path)+"/3DMCG")
+    os.system("rm -rf "+os.path.abspath(Folder_path)+"/Metropolis.e")
+    os.system("rm -rf "+os.path.abspath(Folder_path)+"/tables")
+    os.system("rm -rf "+os.path.abspath(Folder_path)+"/input")
 ###############################################################################
 ### analysis starts from here ...
 ###############################################################################
 
-para_dict = {
-    'Projectile':  "Au",         # projectile nucleus name
-    'Target'    :  "Au",         # target nucleus name
-    'roots'     :  200,         # collision energy (GeV)
-    'seed'      :   -1,          # random seed (-1: system)
-    'baryon_junctions': 1,       # 0: baryon number assumed to be at string end
-                                 # 1: baryon number transported assuming baryon
-                                 # junctions (at smaller x)
-                                 # see arXiv:nucl-th/9602027
-    'lambdaB': 1.0,              # parameter the controls the strength of
-    'lambdaQ': 1.0,
-                                 # the baryon junction stopping
-    'lambdaBs': 1.0,             # fraction of single-to-double string junction stopping
-    'lambdaQs': 1.0,
-    'baryonInStringProb': 0.1,
-    'electric_junctions':  1,
-    'integer_electric_charge': 1,
-    'electricChargeInStringProb': 0.0,
 
-
-    'BG': 17.095,
-    'shadowing_factor':  0.145,     # a shadowning factor for producing strings from multiple scatterings
-    'rapidity_loss_method': 4,
-    'remnant_energy_loss_fraction': 0.611,      # nucleon remnants energy loss fraction (fraction of string's y_loss) [0, 1]
-    'ylossParam4At2': 1.467,
-    'ylossParam4At4': 1.759,
-    'ylossParam4At6': 2.260,
-    'ylossParam4At10': 3.262,
-    'ylossParam4var': 0.356,
-    'evolve_QCD_string_mode': 4,        # string evolution mode
-                                        # 1: deceleration with fixed rapidity loss (m/sigma = 1 fm, dtau = 0.5 fm)
-                                        # 2: deceleration with LEXUS sampled rapidit loss (both dtau and sigma fluctuate)
-                                        # 3: deceleration with LEXUS sampled rapidit loss (m/sigma = 1 fm, dtau fluctuates)
-                                        # 4: deceleration with LEXUS sampled rapidit loss (dtau = 0.5 fm, m/sigma fluctuates)
-}
-
+# Parse user input
 try:
-    nev, path_to_emulator = int(sys.argv[1]), str(sys.argv[2])
+    nev, path_to_parameters, path_to_emulator = int(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3])
 except IndexError:
     printHelp()
     exit(0)
 try:
-    fname_out = str(sys.argv[3])
+    fname_out = str(sys.argv[4])
+    need_fname = False 
 except IndexError:
-    fname_out = "OUT_DICT.dat"
+    need_fname = True 
+try:
+    Fname_out = str(sys.argv[5])
+    need_Fname = False 
+except IndexError:
+    need_Fname = True 
 
-e, B, Q = generate_3DMCGlauber_input(nev, paraDict)
-Predict(B, path_to_emulator, out_fname=fname_out, Nmax=-1)
+# Dynamical import of the custom EMULATOR class
+Emulator = dynamic_import("EMULATOR", os.path.abspath(path_to_emulator+"/EMULATOR.py"))
+
+# Dynamical import of the 3DMCGlauber model parameters dictionnary 
+parameters = dynamic_import("para_dict", path_to_parameters)
+para_dict = parameters.para_dict
+
+# Define output folder and output file name
+if need_fname:
+    fname_out = "OUTPUT_"+para_dict["Projectile"]+ para_dict["Target"]+"_"+str(para_dict["roots"])+"_"+str(nev)+".dat"
+if need_Fname:
+    Fname_out = para_dict["Projectile"]+ para_dict["Target"]+"_"+str(para_dict["roots"])+"_"+str(nev)
+
+# Generate 3DMCGlauber initial conditions in Folder_path
+# and generate predictions using Emulator Folder_path/EM.py 
+# Defined by Class in Folder_path/EMULATOR.py <- where the predict function should be defined. 
+# predict function should take 2D array (Nev, initial Neta = 72) and output 2D array (Nev, final Neta)
+Folder_path, Fname_out = PrepareFolder(path_to_emulator, Fname_out)
+e, B, Q = Generate_3DMCGlauber_input(nev, para_dict, Folder_path)
+Predict(B, path_to_emulator, out_Fname=Fname_out, out_fname=fname_out, Nmax=-1)
+CleanTemporary(Folder_path)
