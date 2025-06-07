eae83d405de7d4fdd6a02b37f92013f8ba9eeb13
Chun Shen <chunshen1987@gmail.com>
2025-06-07 11:32:38 -0400

    update the download script for nuclei configs

--- tables/download_nucleusTables.sh
+++ tables/download_nucleusTables.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
-USERNAME="wayne_state_nuclear_theory"
-REPO_SLUG="GlauberNucleusConfigs"
-BRANCH="main"
+#USERNAME="wayne_state_nuclear_theory"
+#REPO_SLUG="GlauberNucleusConfigs"
+#BRANCH="main"
+#
+## Bitbucket API base url
+#API_BASE_URL="https://api.bitbucket.org/2.0"
+#
+## Define file name
+##FILE_PATH="nucleusConfigurations.tar.gz"
+## Set the name of URL
+#API_FILE_URL="${API_BASE_URL}/repositories/${USERNAME}/${REPO_SLUG}/src/${BRANCH}/${FILE_PATH}"
+#
+#curl -L -o "${FILE_PATH}" "${API_FILE_URL}"
 
-# Bitbucket API base url
-API_BASE_URL="https://api.bitbucket.org/2.0"
+#tar -xf ${FILE_PATH}
+#rm -fr ${FILE_PATH}
+#mv nucleusConfigurations/*.bin.in .
+#rm -fr nucleusConfigurations
 
-# Define file name
-FILE_PATH="nucleusConfigurations.tar.gz"
-# Set the name of URL
-API_FILE_URL="${API_BASE_URL}/repositories/${USERNAME}/${REPO_SLUG}/src/${BRANCH}/${FILE_PATH}"
-
-curl -L -o "${FILE_PATH}" "${API_FILE_URL}"
-
-tar -xf ${FILE_PATH}
-rm -fr ${FILE_PATH}
-mv nucleusConfigurations/*.bin.in .
-rm -fr nucleusConfigurations
+git clone https://Chunshen1987@bitbucket.org/wayne_state_nuclear_theory/glaubernucleusconfigs.git --depth 1
+mv glaubernucleusconfigs/nucleusConfigurations/*.bin.in .
+rm -fr glaubernucleusconfigs
