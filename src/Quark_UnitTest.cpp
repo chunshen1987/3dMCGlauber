554b015073f7297701a596cdb8078f88e6e8584f
Rory Jolliff <156139892+Nobody96103@users.noreply.github.com>
2025-06-23 21:18:31 -0400

    Added test case Quark_UnitTest.cpp
    Added a test case to test baryon, charge, and strange variables.

--- src/Quark_UnitTest.cpp
+++ src/Quark_UnitTest.cpp
@@ -27,3 +27,22 @@ TEST_CASE("Test copy") {
     CHECK(testParticle2.get_p() == p);
     CHECK(testParticle2.get_mass() == 0.0);
 }
+
+TEST_CASE("Test new variables") {
+    MCGlb::SpatialVec x = {1.0, 0.0, -2.0, 3.0};
+    MCGlb::MomentumVec p = {5.0, 0.0, -4.0, 3.0};
+    real b1 = 1;
+    real c1 = 0;
+    real s1 = -1;
+    real b2 = 0;
+    real c2 = 1;
+    real s2 = 0;
+    MCGlb::Quark testParticle1(x, p, b1, c1, s1);
+    MCGlb::Quark testParticle2(x, p, b2, c2, s2);
+    CHECK(testParticle1.get_baryon() == b1);
+    CHECK(testParticle1.get_charge() == c1);
+    CHECK(testParticle1.get_strange() == s1);
+    CHECK(testParticle2.get_baryon() == b2);
+    CHECK(testParticle2.get_charge() == c2);
+    CHECK(testParticle2.get_strange() == s2);
+}
